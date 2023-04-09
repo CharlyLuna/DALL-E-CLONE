4444af62bd9abf68c408c98209b09f61db883d66
CharlyLuna <cluna10@ucol.mx>
2023-04-09 15:23:43 -0600

    Create structure of image cards

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,4 +1,5 @@
 import { surpriseMePrompts } from '../constants'
+import FileSaver from 'file-saver'
 
 export const getRandomPrompt = (prompt) => {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length)
@@ -6,4 +7,8 @@ export const getRandomPrompt = (prompt) => {
   // Check to not return the same random prompt two time in a row
   if (randomPrompt === prompt) return getRandomPrompt(prompt)
   return randomPrompt
+}
+
+export const downloadImage = async (_id, photo) => {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`)
 }
